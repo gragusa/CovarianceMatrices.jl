3865c9a76e6016eaaeeb695dedcea38ea79544f1
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-12-30 15:53:14 +0100

    Added stderr methods from RobustVariance

--- src/HC.jl
+++ src/HC.jl
@@ -1,7 +1,7 @@
-function vcov(X::AbstractMatrix, k::HC)	
+function vcov(X::AbstractMatrix, k::HC)
 	N, p = size(X)
 	return X'X/N
-end 	
+end
 
 typealias GenLinMod GeneralizedLinearModel
 
@@ -11,47 +11,47 @@ adjfactor(LM::GenLinMod, k::HC2) = sqrt(1./(1.-hatmatrix(LM)))
 adjfactor(LM::GenLinMod, k::HC3) = 1./(1.-hatmatrix(LM))
 
 
-function adjfactor(LM::GenLinMod, k::HC4) 
+function adjfactor(LM::GenLinMod, k::HC4)
 	hᵢ = hatmatrix(LM)
 	h  = mean(hᵢ)
 	δ  = min(4, hᵢ/h)
 	sqrt(1./(1.-hᵢ).^δ)
-end 
+end
 
 nobs(LM::GenLinMod) = length(LM.rr.y)
 
-function bread(LM::GenLinMod)   
+function bread(LM::GenLinMod)
 	nobs(LM)*inv(cholfact(LM.pp))
-end 
+end
 
-function residuals(LM::GenLinMod, k::HC)	
+function residuals(LM::GenLinMod, k::HC)
 	ε = (LM.rr.wrkresid.*LM.rr.wrkwts).*adjfactor(LM, k)
 	diagm(ε)
-end 	
+end
 
-function residuals(LM::GenLinMod, k::HAC) 
+function residuals(LM::GenLinMod, k::HAC)
 	ε = (LM.rr.wrkresid.*LM.rr.wrkwts)
 	diagm(ε)
-end 	
+end
 
-function hatmatrix(LM::GenLinMod) 
-	X = ModelMatrix(LM).*sqrt(LM.rr.wrkwts)	
+function hatmatrix(LM::GenLinMod)
+	X = ModelMatrix(LM).*sqrt(LM.rr.wrkwts)
 	diag(X*inv(cholfact(LM.pp))*X')
-end 
+end
 
-function meat(LM::GenLinMod, k::RobustVariance) 
-	ε = residuals(LM, k) 
+function meat(LM::GenLinMod, k::RobustVariance)
+	ε = residuals(LM, k)
 	X = ModelMatrix(LM)
 	vcov(ε*X, k)
 end
 
-function vcov(LM::DataFrameRegressionModel, k::RobustVariance)	
+function vcov(LM::DataFrameRegressionModel, k::RobustVariance)
 	Ω = meat(LM.model, k)
 	Q = bread(LM.model)
 	Q*Ω*Q/nobs(LM)
 end
 
-stderr(LM::DataFrameRegressionModel, k::HAC) = sqrt(diag(vcov(LM, k)))
+stderr(LM::DataFrameRegressionModel, k::RobustVariance) = sqrt(diag(vcov(LM, k)))
 
 
 
