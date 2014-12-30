5fa4997cfe4b4f89b1c7231517caf442d0fce37c
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2014-12-30 17:31:54 +0100

    Remove trailing spaces

--- src/VCOV.jl
+++ src/VCOV.jl
@@ -9,7 +9,7 @@ using PDMats
 const π²=π^2
 const sixπ = 6*π
 
-export QuadraticSpectralKernel, TruncatedKernel, ParzenKernel, BartlettKernel, 
+export QuadraticSpectralKernel, TruncatedKernel, ParzenKernel, BartlettKernel,
        HC0, HC1, HC2, HC3, HC4,
        vcov, kernel, bread, meat
 
@@ -17,15 +17,13 @@ abstract RobustVariance
 abstract HAC <: RobustVariance
 abstract HC  <: RobustVariance
 
-type HC0  <: HC end 
-type HC1  <: HC end 
-type HC2  <: HC end 
-type HC3  <: HC end 
-type HC4  <: HC end 
-type HC4m <: HC end 
-type HC5  <: HC end 
-
-
+type HC0  <: HC end
+type HC1  <: HC end
+type HC2  <: HC end
+type HC3  <: HC end
+type HC4  <: HC end
+type HC4m <: HC end
+type HC5  <: HC end
 
 include("HAC.jl")
 include("optimalbw.jl")
