7af331c0714a9ed34687ec2b2b16c2f41fcb2700
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2015-01-02 00:45:27 +0100

    Added bwAndrews for optimal bw selection

--- src/optimalbw.jl
+++ src/optimalbw.jl
@@ -17,53 +17,82 @@ function lag!{T}(Yl::Array{T, 2}, Y::Array{T, 1}, p::Int64)
 end
 
 function ar{T}(Y::Array{T, 2}, lag::Int64)
-	N, p = size(Y)
-	Yl = Array(T, N-lag, lag+1)
-	ρ  = Array(T, p, lag)
-	σ² = Array(T, p)
-	for j = 1:p
-		lag!(Yl, Y[:,j], lag)
-		Yt          = Y[lag+1:end,j]
-		θ           = (Yl\Yt)
-		ρ[j, 1:lag] = θ[1:lag]
-		ε         = Yt - Yl*θ
-		σ²[j] = (ε'ε/N)[1]
-	end
-	return ρ, σ²
+	   N, p = size(Y)
+	   Yl = Array(T, N-lag, lag+1)
+	   ρ  = Array(T, p, lag)
+	   σ² = Array(T, p)
+	   for j = 1:p
+		      lag!(Yl, Y[:,j], lag)
+		      Yt          = Y[lag+1:end,j]
+		      θ           = (Yl\Yt)
+		      ρ[j, 1:lag] = θ[1:lag]
+		      ε           = Yt - Yl*θ
+		      σ²[j]       = (ε'ε/N)[1]
+	   end
+	   return ρ, σ²
 end
 
+function arma{T}(Y::Array{T,2})
+    ## Estimate an ARMA(1,1) for each column of Y
+end 
 
-function getalpha(X::AbstractMatrix)
-  ρ, σ² = ar(X, 1)
-  σ⁴ = (σ²).^2
-  nm   = 4.*ρ.^2.*σ⁴./(1-ρ).^6.*(1+ρ).^2
-  dn   = σ⁴./(1-ρ).^4
-  α₁ = sum(nm)/sum(dn)
-  nm   = 4.*ρ.^2.*σ⁴./(1-ρ).^8
-  α₂ = sum(nm)/sum(dn)
-  return α₁, α₂
+ar{T}(Y::Array{T, 2}) = ar(Y, 1)
+
+d_bw_andrews = [:TruncatedKernel         => :(0.6611*(α₂*N)^(0.2)),
+                :BartlettKernel          => :(1.1447*(α₁*N)^(1/3)),
+                :ParzenKernel            => :(2.6614*(α₂*N)^(0.2)),
+                :QuadraticSpectralKernel => :(1.3221*(α₂*N)^(0.2))]
+
+for tty in [:TruncatedKernel, :BartlettKernel, :ParzenKernel, :QuadraticSpectralKernel]
+    @eval  $:(bw_andrews)(k::($tty), α₁, α₂, N) = $(d_bw_andrews[tty])
+end 
+
+function getalpha(X::AbstractMatrix, approx::Symbol)
+    ## @assert approx == :ar ## || approx == :arma
+    if approx == :ar
+        ρ, σ² = ar(X)
+        σ⁴    = (σ²).^2
+        nm    = 4.*ρ.^2.*σ⁴./((1-ρ).^6.*(1+ρ).^2)
+        dn    = σ⁴./(1-ρ).^4
+        α₁    = sum(nm)/sum(dn)
+        nm    = 4.*ρ.^2.*σ⁴./(1-ρ).^8
+        α₂    = sum(nm)/sum(dn)
+    # elseif approx == :arma  [TODO]
+    end 
+    return α₁, α₂
 end
 
 function optimalbw_ar_one(X::AbstractMatrix, k::TruncatedKernel)
-	α₁, α₂ = getalpha(X)
-	T, p = size(X)
-	return .6611*(α₂*T)^(1/5)
+	   α₁, α₂ = getalpha(X)
+	   T, p   = size(X)
+	   return .6611*(α₂*T)^(1/5)
 end
 
 function optimalbw_ar_one(X::AbstractMatrix, k::BartlettKernel)
-	α₁, α₂ = getalpha(X)
-	T, p = size(X)
-	return 1.1447*(α₁*T)^(1/3)
+	   α₁, α₂ = getalpha(X)
+	   T, p   = size(X)
+	   return 1.1447*(α₁*T)^(1/3)
 end
 
 function optimalbw_ar_one(X::AbstractMatrix, k::ParzenKernel)
-	α₁, α₂ = getalpha(X)
-	T, p = size(X)
-	return 2.6614*(α₂*T)^(1/5)
+	   α₁, α₂ = getalpha(X)
+	   T, p   = size(X)
+	   return 2.6614*(α₂*T)^(1/5)
 end
 
 function optimalbw_ar_one(X::AbstractMatrix, k::QuadraticSpectralKernel)
-	α₁, α₂ = getalpha(X)
-	T, p = size(X)
-	return 1.3221*(α₂*T)^(1/5)
+	   α₁, α₂ = getalpha(X)
+	   T, p = size(X)
+	   return 1.3221*(α₂*T)^(1/5)
 end
+
+function bwAndrews{T}(X::Array{T, 2}, k::HAC; prewhite::Bool = false, approx::Symbol = :ar)
+    ## if prewhite
+        ## Not yet implemented --- need VAR(1)
+    ## end
+    α₁, α₂ = getalpha(X, approx)
+    N,  p  = size(X)
+    return bw_andrews(k, α₁, α₂, N)
+end 
+    
+    
