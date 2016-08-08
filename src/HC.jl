e5fb98e81dc20a7a8fef2a6652bac6e28bcd9ca6
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2016-08-09 00:50:34 +0200

    Added compatibility layer for cholfact

--- src/HC.jl
+++ src/HC.jl
@@ -146,6 +146,10 @@ function getqii(v::CRHC3, e, X, A, bstarts)
     return e
 end
 
+if VERSION < v"0.5.0"
+    Base.cholfact(A::Symmetric, args...) = cholfact(A.data, Symbol(A.uplo), args...)
+end
+
 function getqii(v::CRHC2, e, X, A, bstarts)
     for j in 1:length(bstarts)
         rnge = bstarts[j]
