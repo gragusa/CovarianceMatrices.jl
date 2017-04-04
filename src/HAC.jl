53fca4219941fe074038c41d1a563375687e86ce
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2017-04-04 15:09:51 +0200

    Fix depwarn (9)

--- src/HAC.jl
+++ src/HAC.jl
@@ -100,7 +100,7 @@ immutable QuadraticSpectralKernel{G<:BandwidthType, F<:Function} <: HAC{G}
     weights::Array{Float64,1}
 end
 
-immutable VARHAC <: HAC
+immutable VARHAC{G} 
     imax::Int64
     ilag::Int64
     imodel::Int64
