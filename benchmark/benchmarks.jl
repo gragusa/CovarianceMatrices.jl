020f359753ccbd48f3eae20bdc7dbf8e10a659d1
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2018-02-16 22:13:04 +0100

    Fix glm calls

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,12 +1,12 @@
 using PkgBenchmark
 using CovarianceMatrices
-
+using CSV
 
 @benchgroup "HAC - Fixed Bandwidth" begin
     @benchgroup "No prewithen" begin
         nf = string(Pkg.dir("CovarianceMatrices"), "/test/ols_hac.csv")
-        df = readtable(nf)
-        lm1 = glm(y~x+w, df, Normal(), IdentityLink())
+        df = CSV.read(nf)
+	      lm1 = glm(@formula(y~x+w), df, Normal(), IdentityLink())
         @bench "Truncated Kernel" vcov(lm1, TruncatedKernel(1.0), prewhite = false)
         @bench "Quadratic Spectral Kernel" vcov(lm1, QuadraticSpectralKernel(1.0), prewhite = false)
         @bench "Parzen Kernel" vcov(lm1, ParzenKernel(1.0), prewhite = false)
@@ -15,8 +15,8 @@ using CovarianceMatrices
 
     @benchgroup "Prewithen" begin
         nf = string(Pkg.dir("CovarianceMatrices"), "/test/ols_hac.csv")
-        df = readtable(nf)
-        lm1 = glm(y~x+w, df, Normal(), IdentityLink())
+	      df = CSV.read(nf)
+        lm1 = glm(@formula(y~x+w), df, Normal(), IdentityLink())
         @bench "Truncated Kernel" vcov(lm1, TruncatedKernel(1.0), prewhite = true)
         @bench "Quadratic Spectral Kernel" vcov(lm1, QuadraticSpectralKernel(1.0), prewhite = true)
         @bench "Parzen Kernel" vcov(lm1, ParzenKernel(1.0), prewhite = true)
@@ -29,8 +29,8 @@ end
 @benchgroup "HAC - Optimal Bandwidth" begin
     @benchgroup "No prewithen" begin
         nf = string(Pkg.dir("CovarianceMatrices"), "/test/ols_hac.csv")
-        df = readtable(nf)
-        lm1 = glm(y~x+w, df, Normal(), IdentityLink())
+        df = CSV.read(nf)
+        lm1 = glm(@formula(y~x+w), df, Normal(), IdentityLink())
         @bench "Truncated Kernel" vcov(lm1, TruncatedKernel(), prewhite = false)
         @bench "Quadratic Spectral Kernel" vcov(lm1, QuadraticSpectralKernel(), prewhite = false)
         @bench "Parzen Kernel" vcov(lm1, ParzenKernel(), prewhite = false)
@@ -39,8 +39,8 @@ end
 
     @benchgroup "Prewithen" begin
         nf = string(Pkg.dir("CovarianceMatrices"), "/test/ols_hac.csv")
-        df = readtable(nf)
-        lm1 = glm(y~x+w, df, Normal(), IdentityLink()) 
+	      df = CSV.read(nf)
+        lm1 = glm(@formula(y~x+w), df, Normal(), IdentityLink())
         @bench "Truncated Kernel" vcov(lm1, TruncatedKernel(), prewhite = true)
         @bench "Quadratic Spectral Kernel" vcov(lm1, QuadraticSpectralKernel(), prewhite = true)
         @bench "Parzen Kernel" vcov(lm1, ParzenKernel(), prewhite = true)
