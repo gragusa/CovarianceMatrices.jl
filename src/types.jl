0bfb07dd12269037156d60b57143561144db4012
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2019-01-14 19:34:36 +0100

    Add CovarianceMatrix type

--- src/types.jl
+++ src/types.jl
@@ -1,3 +1,7 @@
+const WFLOAT = Sys.WORD_SIZE == 64 ? Float64 : Float32
+
+
+
 #=========
 Abstraction
 ==========#
@@ -5,7 +9,7 @@ Abstraction
 abstract type RobustVariance end
 abstract type HAC{G} <: RobustVariance end
 abstract type HC <: RobustVariance end
-abstract type CRHC <: RobustVariance end
+abstract type CRHC{V} <: RobustVariance end
 
 
 #=========
@@ -80,18 +84,50 @@ struct HC5  <: HC end
 
 #const CLVector{T<:Integer} = DenseArray{T,1}
 
-mutable struct CRHC0{V<:AbstractVector}  <: CRHC
+mutable struct CRHC0{V<:AbstractVector}  <: CRHC{V}
     cl::V
 end
 
-mutable struct CRHC1{V<:AbstractVector}  <: CRHC
+mutable struct CRHC1{V<:AbstractVector}  <: CRHC{V}
     cl::V
 end
 
-mutable struct CRHC2{V<:AbstractVector}  <: CRHC
+mutable struct CRHC2{V<:AbstractVector}  <: CRHC{V}
     cl::V
 end
 
-mutable struct CRHC3{V<:AbstractVector}  <: CRHC
+mutable struct CRHC3{V<:AbstractVector}  <: CRHC{V}
     cl::V
 end
+
+struct CovarianceMatrix{T2<:Factorization, T3<:CovarianceMatrices.RobustVariance, F1, T1<:AbstractMatrix{F1}} <: AbstractMatrix{F1}
+    F::T2       ## Factorization
+    K::T3       ## RobustVariance, e.g. HC0()
+    V::T1       ## The Variance Covariance
+end
+
+
+
+#=======
+Caches
+=======#
+
+abstract type AbstractCache end
+
+struct HACCache{TYPE, F<:AbstractMatrix, V<:AbstractVector} <: AbstractCache
+    prew::TYPE
+    X_demean::F ## Should call this q n_origin x p
+    YY::F
+    XX::F
+    Y_lagged::F
+    X_lagged::F
+    μ::F        ## p x 1
+    Q::F        ## p x p
+    V::F        ## p x p
+    D::F       ## p x p
+    U::V
+    ρ::V
+    σ⁴::V
+    u::F
+end
+
