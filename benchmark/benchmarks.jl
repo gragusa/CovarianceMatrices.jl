73ec293471000e0c289ba45af1353d0c1896e0f3
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2019-10-21 18:55:15 +0200

    Use larger bandwidth for fixed HAC

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,7 +6,13 @@ using BenchmarkTools
 
 Random.seed!(1)
 
-df = DataFrame(y = randn(300*50))
+u = zeros(3000)
+for j in 1:2999
+    u[j+1] = 0.97*u[j] + randn()
+end
+
+df = DataFrame(y = randn(300*50) .+ u[1500:3000])
+
 for j in Symbol.("x".*string.(collect(1:5)))
     df[j] = randn(300*50)
 end
@@ -15,17 +21,16 @@ df[:cluster] = repeat(1:50, inner = [300])
 
 frm = @formula(y ~ x1 + x2 + x3 + x4 + x5)
 lm1 = glm(frm, df, Normal(), IdentityLink())
-k_fix = TruncatedKernel(2)
+k_fix = TruncatedKernel(10)
 k_opt_andrews = TruncatedKernel()
-k_opt_newey = TruncatedKernel(NeweyWest)
+#k_opt_newey = TruncatedKernel(NeweyWest)
 
 SUITE = BenchmarkGroup()
 SUITE["HAC"] = BenchmarkGroup(["Optimal Uncached", "Fixed Uncached"])
 SUITE["HAC"]["Truncated Optimal Andrews"] = @benchmarkable vcov(lm1, $k_opt_andrews)
-SUITE["HAC"]["Truncated Optimal Andrews"] = @benchmarkable vcov(lm1, $k_opt_newey)
 SUITE["HAC"]["Truncated Fixed "] = @benchmarkable vcov(lm1, $k_fix)
 
-k_fix = ParzenKernel(2)
+k_fix = ParzenKernel(10)
 k_opt_andrews = ParzenKernel()
 k_opt_newey = ParzenKernel(NeweyWest)
 
