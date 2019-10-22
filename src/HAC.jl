ebd4aea85138223cfecb35ec98430ab20e1a31ba
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2019-10-22 10:27:54 +0200

    Fixed bwNewey: was missing prewhite information

--- src/HAC.jl
+++ src/HAC.jl
@@ -18,11 +18,11 @@ ParzenKernel(x::Type{Andrews}) = PRK(Optimal{Andrews}(), Array{WFLOAT}(undef,1),
 TukeyHanningKernel(x::Type{Andrews}) = THK(Optimal{Andrews}(), Array{WFLOAT}(undef,1), Array{WFLOAT}(undef,0))
 QuadraticSpectralKernel(x::Type{Andrews}) = QSK(Optimal{Andrews}(), Array{WFLOAT}(undef,1), Array{WFLOAT}(undef,0))
 
-TruncatedKernel(bw::Number) = TRK(Fixed(), [float(bw)], Array{WFLOAT}(undef,0))
-BartlettKernel(bw::Number) = BTK(Fixed(), [float(bw)], Array{WFLOAT}(undef,0))
-ParzenKernel(bw::Number) = PRK(Fixed(), [float(bw)], Array{WFLOAT}(undef,0))
-TukeyHanningKernel(bw::Number) = THK(Fixed(), [float(bw)], Array{WFLOAT}(undef,0))
-QuadraticSpectralKernel(bw::Number) = QSK(Fixed(), [float(bw)], Array{WFLOAT}(undef,0))
+TruncatedKernel(bw::Number) = TRK(Fixed(), Array{WFLOAT}(undef, 1), Array{WFLOAT}(undef,0))
+BartlettKernel(bw::Number) = BTK(Fixed(), Array{WFLOAT}(undef, 1), Array{WFLOAT}(undef,0))
+ParzenKernel(bw::Number) = PRK(Fixed(), Array{WFLOAT}(undef, 1), Array{WFLOAT}(undef,0))
+TukeyHanningKernel(bw::Number) = THK(Fixed(), Array{WFLOAT}(undef, 1), Array{WFLOAT}(undef,0))
+QuadraticSpectralKernel(bw::Number) = QSK(Fixed(), Array{WFLOAT}(undef, 1), Array{WFLOAT}(undef,0))
 
 covindices(k::T, n) where T<:QuadraticSpectralKernel = Iterators.filter(x -> x!=0, -n:n)
 covindices(k::HAC, n) = Iterators.filter(x -> x!=0, -floor(Int, k.bw[1]):floor(Int, k.bw[1]))
@@ -30,20 +30,24 @@ covindices(k::HAC, n) = Iterators.filter(x -> x!=0, -floor(Int, k.bw[1]):floor(I
 function Γ(A::Matrix{T}, j) where T
     n, p = size(A)
     Q = zeros(T, p, p)
-    if j >= 0
-        for h=1:p, s = 1:h
-            for t = j+1:n
-                @inbounds Q[s, h] = Q[s, h] + A[t, s]*A[t-j, h]
-            end
-        end
-    elseif j<0
-        for h=1:p, s = 1:h
-            for t = -j+1:n
-                @inbounds Q[s,h] = Q[s ,h] + A[t+j, s]*A[t,h]
-            end
+    Γsign!(Q, A, Val{j>0})
+    return Q
+end
+
+function Γsign!(Q, A, ::Type{Val{true}})
+    for h=1:p, s = 1:h
+        for t = j+1:n
+            @inbounds Q[s, h] = Q[s, h] + A[t, s]*A[t-j, h]
         end
     end
-    return Q
+end
+
+function Γsign!(Q, A, ::Type{Val{false}})
+    for h=1:p, s = 1:h
+        for t = -j+1:n
+            @inbounds Q[s,h] = Q[s ,h] + A[t+j, s]*A[t,h]
+        end
+    end
 end
 
 function getbandwidth(k::HAC{Optimal{T}}, m::AbstractMatrix{F}) where {T<:OptimalBandwidth,F}
@@ -78,14 +82,15 @@ function kernel(k::QuadraticSpectralKernel, x::Real)
     iszero(x) ? 1.0 : (z = 1.2*π*x; 3*(sin(z)/z-cos(z))*(1/z)^2)
 end
 
-function setupkernelweights!(k, m::AbstractMatrix{T}) where T
+function setupkernelweights!(k, m::AbstractMatrix)
     n, p = size(m)
+    ## k.weights is WFLOAT
     if isempty(k.weights)
         for j in 1:p
-            push!(k.weights, one(T))
+            push!(k.weights, one(WFLOAT))
         end
     elseif all(iszero.(k.weights))
-        fill!(k.weights, one(T))
+        fill!(k.weights, one(WFLOAT))
     end
 end
 
@@ -128,10 +133,10 @@ end
 ## Optimal bandwidth
 ##
 ##############################################################################
-optimal_bw(k::HAC, optype::T, mm) where T<:NeweyWest = bwNeweyWest(k, mm)
-optimal_bw(k::HAC, opttype::T, mm) where T<:Andrews = bwAndrews(k, mm)
+optimal_bw(k::HAC, optype::T, mm; prewhite::Bool=false) where T<:NeweyWest = bwNeweyWest(k, mm, prewhite)
+optimal_bw(k::HAC, opttype::T, mm; prewhite::Bool=false) where T<:Andrews = bwAndrews(k, mm, prewhite)
 
-function bwAndrews(k::HAC, mm)
+function bwAndrews(k::HAC, mm, prewhite)
     isempty(k.weights) && (fill!(k.weights, 1.0))
     n, p  = size(mm)
     a1, a2 = getalpha(k, mm)
@@ -140,11 +145,11 @@ function bwAndrews(k::HAC, mm)
 end
 
 ## ---> Andrews Optimal bandwidth <---
-d_bw_andrews = Dict(:TruncatedKernel         => :(0.6611*(a2*n)^(0.2)),
+d_bw_andrews = Dict(:TruncatedKernel         => :(0.6611*(a2*n)^(1/5)),
                     :BartlettKernel          => :(1.1447*(a1*n)^(1/3)),
-                    :ParzenKernel            => :(2.6614*(a2*n)^(0.2)),
-                    :TukeyHanningKernel      => :(1.7462*(a2*n)^(0.2)),
-                    :QuadraticSpectralKernel => :(1.3221*(a2*n)^(0.2)))
+                    :ParzenKernel            => :(2.6614*(a2*n)^(1/5)),
+                    :TukeyHanningKernel      => :(1.7462*(a2*n)^(1/5)),
+                    :QuadraticSpectralKernel => :(1.3221*(a2*n)^(1/5)))
 
 for kerneltype in [:TruncatedKernel, :BartlettKernel, :ParzenKernel, :TukeyHanningKernel, :QuadraticSpectralKernel]
     @eval $:(bw_andrews)(k::($kerneltype), a1, a2, n) = $(d_bw_andrews[kerneltype])
@@ -153,6 +158,7 @@ end
 function getalpha(k, mm)
     w = k.weights
     rho, σ⁴ = fit_ar(mm)
+    ##rho2 = (rho.^2)
     nm = 4.0.*(rho.^2).*σ⁴./(((1.0.-rho).^6).*((1.0.+rho).^2))
     dn = σ⁴./(1.0.-rho).^4
     α₁ = sum(w.*nm)/sum(w.*dn)
@@ -161,9 +167,9 @@ function getalpha(k, mm)
     return α₁, α₂
 end
 
-function bwNeweyWest(k::HAC, mm; prewhite=false)
+function bwNeweyWest(k::HAC, mm, prewhite)
     n, p = size(mm)
-    l = getrates(k, mm, prewhite=prewhite)
+    l = getrates(k, mm, prewhite)
     w = k.weights
     xm = mm*w
     a = map(j -> dot(xm[1:n-j], xm[j+1:n])/n, 0:l)::Array{Float64, 1}
@@ -174,7 +180,7 @@ function bwNeweyWest(k::HAC, mm; prewhite=false)
     k.bw[1] = bwnw(k, a0, a1, a2)*(n+prewhite)^growthrate(k)
 end
 
-function getrates(k, mm; prewhite = false)
+function getrates(k, mm, prewhite)
     n, p = size(mm)
     lrate = lagtruncation(k)
     adj = prewhite ? 3 : 4
