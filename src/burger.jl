a6af3670a4f3b6ae69c72704a86276c1c03ba43f
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2024-10-15 11:57:06 +0000

    Remove burger

--- src/burger.jl
+++ src/burger.jl
@@ -1,11 +1,11 @@
-function burger(K::AVarEstimator, momentmatrix::AbstractMatrix{T}, bun::Union{AbstractMatrix{F}, Factorization{F}}; demean=false, prewhite::Bool=false, dof::Int64=0) where {T<:Real, F<:Real}
-    kwargs = (demean=demean, prewhite=prewhite, dof=dof, unscaled=true)
-    P = patty(K, m; kwargs...)
-    ## Form A^{-1}BA^{-1}'
-    return (bun\P)/bun
-end
+# function burger(K::AVarEstimator, momentmatrix::AbstractMatrix{T}, bun::Union{AbstractMatrix{F}, Factorization{F}}; demean=false, prewhite::Bool=false, dof::Int64=0) where {T<:Real, F<:Real}
+#     kwargs = (demean=demean, prewhite=prewhite, dof=dof, unscaled=true)
+#     P = patty(K, m; kwargs...)
+#     ## Form A^{-1}BA^{-1}'
+#     return (bun\P)/bun
+# end
 
-patty(K::AVarEstimator, m::AbstractMatrix; kwargs...) = aVar(K, m; kwargs...)
+# patty(K::AVarEstimator, m::AbstractMatrix; kwargs...) = aVar(K, m; kwargs...)
 
 
 
